0c656527e3efa43eb81c85b754c77bc568222583
User <User@koristuvacs-MBP.Dlink>
2018-09-23 13:38:07 +0300

    Lab 1 is complite

--- Object_o/Lab1/Lab1/Program.cs
+++ Object_o/Lab1/Lab1/Program.cs
@@ -1,12 +1,57 @@
-﻿using System;
+﻿// Lab1
+
+using System;
 
 namespace Lab1
 {
     class MainClass
     {
+        private static Array MyArray = new Array();
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MyArray.square();
+            MyArray.logout();
+        }
+    }
+    class Array {
+        private double[] array;
+        private double[] arrayNew;
+
+        private int length;
+
+        private Random rnd = new Random();
+
+        // Criating arrais
+        private void ArrayLength () {
+            Console.WriteLine("\n Write leangth of array");
+            length = Convert.ToInt32(Console.ReadLine());
+            array = new double[length];
+            arrayNew = new double[length];
+        }
+
+        // Fulling arrais with data
+        private void init()
+        {
+            for (int i = 0; i < length; i++)
+                array[i] = (double)rnd.Next(1, 100);
+        }
+
+        // Doing math =)
+        public void square () {
+            for (int i = 0; i < length; i++)
+                arrayNew[i] = Math.Pow(array[i],2);
+        }
+
+        // Printing data out
+        public void logout () {
+            for (int i = 0; i < length; i++)
+                Console.WriteLine("\n" + arrayNew[i]);
+        }
+
+        public Array () {
+            ArrayLength();
+            init();
         }
     }
 }
