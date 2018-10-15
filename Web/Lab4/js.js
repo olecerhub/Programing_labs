731a71c61e2d55eb19aa95dc745e0a1715da42df
User <User@koristuvacs-MBP.Dlink>
2018-10-15 14:53:29 +0300

    WEB Lab4

---
+++ Web/Lab4/js.js
@@ -0,0 +1,20 @@
+class Bookmarks {
+    constructor(id, name, description, link) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.link = link;
+
+        this.str = "<li id='" + this.id + "'><a href='" + this.link + "'><b>" + this.name + ": </b>" + this.description + "</a></li>";
+        $("#ForBookmarks").html(this.str);
+    }
+}
+
+function AddLisner () {
+  let bookmark = new Bookmarks (prompt("Id:"),prompt("Name:"),prompt("Description:"),prompt("Link:"));
+}
+
+function DeleteLisner () {
+  var length = $("#ForBookmarks li").length;
+  $("#ForBookmarks li:nth-child(" + length + ")").html("");
+}
